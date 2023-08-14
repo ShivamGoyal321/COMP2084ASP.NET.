15ba13ee79a8d7e4c2a9944c49d799f47b215b54
ShivamGoyal321 <122632280+ShivamGoyal321@users.noreply.github.com>
2023-08-14 13:17:34 -0700

    Add files via upload

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace LabWebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        // Add more DbSet properties for other entity types if needed
+    }
+}
